d9e2cdcd8b3a529c497a7b6e6feb4ab1e0952e75
Andreas Spiropoulos <spiroan2000@windowslive.com>
2020-11-16 14:36:57 +0200

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
@@ -17,11 +18,13 @@ namespace API.Controllers
 		}
 
 		[HttpGet]
+		[AllowAnonymous]
 		public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
 		{
 			return await _context.Users.ToListAsync();
 		}
 
+		[Authorize]
 		[HttpGet("{id}")]
 		public async Task<ActionResult<AppUser>> GetUsers(int id)
 		{
